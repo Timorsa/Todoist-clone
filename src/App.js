ded8e2d3efdcff2a4d54777786fc688b05f6b872
Timor Safadi <48487764+Timorsa@users.noreply.github.com>
2020-03-06 17:40:57 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header } from './components/layout/Header';
+import { Content } from './components/layout/Content';
+
+import './App.scss';
+
+export const App = () => (
+	<div className="App">
+		<Header />
+		<Content />
+	</div>
+);
+
+export default App;
